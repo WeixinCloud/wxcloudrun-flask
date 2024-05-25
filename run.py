5b7af7e3a9c63b2ce8880c538368cb33a9d9122e
zuoyuan.zy <zuoyuan.zy@bytedance.com>
2024-05-25 21:06:05 +0800

    add log

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 # 创建应用实例
 import sys
+import logging
 
 from wxcloudrun import app
+
+logging.basicConfig(level=logging.INFO)
 
 # 启动Flask Web服务
 if __name__ == '__main__':
--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -10,7 +10,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-
 @app.route('/')
 def index():
     """
